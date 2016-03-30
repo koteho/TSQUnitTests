ed014cc780c52d1470fd269e8c80f623702dc22a
Pavlo Kryvonis <PavloK@digitalgamingcorp.com>
2016-03-30 15:24:07 -0400

    First alpha v0.1

--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/app.js
@@ -5,5 +5,6 @@ function doSomethingElse() {
     var result = doSomething(2, 3, 4);
     return result;
 }
+console.log("1 + 2 + 3 =");
 console.log(doSomething(1, 2, 3));
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/TypeScriptHTMLApp1/test.ts
+++ TypeScriptHTMLApp1/TypeScriptHTMLApp1/test.ts
@@ -9,15 +9,15 @@ test('doSomething()', function () {
     var expected = 3
     // Assert
     equal(result , expected, "they should match");
-    equal(doSomething(2, 4, 6), 12, "they should match");
-    equal(doSomething(1, 5, 7), 13, "they should match");
-    equal(doSomething(4, 8, 9), 21, "they should match");
-    equal(doSomething(-1, 2, 4), 5, "they should match");
+    equal(doSomething(2, 4, 6), 12, "Test 1: 2+4+6 should be 12");
+    equal(doSomething(1, 5, 7), 13, "Test 2: 1+5+7 should be 13");
+    equal(doSomething(4, 8, 9), 21, "Test 3: 4+8+9 should be 21");
+    equal(doSomething(-1, 2, 4), 5, "Test 4: -1+2+4 should be 5");
     
     // intentionally wrong
-    equal(doSomething(1, 4, 5), 0, "intentionally wrong");
+    equal(doSomething(1, 4, 5), 0, "Test 5: intentionally wrong");
 });
 
 QUnit.test('doSomethingElseTest()', function () {
-    equal(doSomethingElse(), '91', "they should match");
+    equal(doSomethingElse(), '9', "doSomething(2, 3, 4); should be 9");
 })
